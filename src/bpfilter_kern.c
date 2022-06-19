4f9be053f3a2d16a47998670d42986cd1ab3bf6d
Lucas Bleme <andreybleme1@gmail.com>
2022-06-18 23:37:10 -0300

    Fix bpf headers

--- src/bpfilter_kern.c
+++ src/bpfilter_kern.c
@@ -1,43 +1,60 @@
+/* Linux headers for bpf and networking */
+#include <linux/bpf.h>
+#include <linux/in.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+
+/* libbpf header files */
 #include "bpf/bpf_helpers.h"
 #include "bpf/bpf_endian.h"
 
-/* bpf and networking linux headers */
-#include <linux/bpf.h>
-#include <linux/in.h>
-#include <linux/ip.h>
-#include <linux/udp.h>
-#include <linux/if_ether.h>
+/* Map that controls connections counter by source IP */
+struct bpf_map_def SEC("maps") counters = {
+
+    .type = BPF_MAP_TYPE_HASH,
+
+    /* A 32 bits representing the source IPv4 address */
+    .key_size = sizeof(__be32),
+
+    /* A integer counter of the connections */
+    .value_size = sizeof(int),
+
+    .max_entries = 10,
+};
 
 SEC("bpfilter")
 int handle_tp(struct xdp_md *ctx)
-{	
-	bpf_printk("[XDP] starting pkt filter\n");
+{
+  bpf_printk("starting xdp ip filter\n");
 
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data = (void *)(long)ctx->data;
-	struct ethhdr *eth = data;
+  void *data_end = (void *)(long)ctx->data_end;
+  void *data     = (void *)(long)ctx->data;
+  struct ethhdr *eth = data;
 
-	// check packet size
-	if (eth + 1 > data_end) {
-		return XDP_PASS;
-	}
+  // check packet size
+  if (eth + 1 > data_end) {
+    return XDP_PASS;
+  }
 
-	// get the source address of the packet
-	struct iphdr *iph = data + sizeof(struct ethhdr);
-	if (iph + 1 > data_end) {
-		return XDP_PASS;
-	}
+  // get the source address of the packet
+  struct iphdr *iph = data + sizeof(struct ethhdr);
+  if (iph + 1 > data_end) {
+    return XDP_PASS;
+  }
 
-	__u32 ip_src = iph->saddr;
-	bpf_printk("[XDP] source ip %u\n", ip_src);
+  __u32 ip_src = iph->saddr;
+  bpf_printk("source ip: %u\n", ip_src);
 
-	__u32 ip_dst = iph->daddr;
-	bpf_printk("[XDP] destination ip %u\n", ip_dst);
+  __u32 ip_dst = iph->daddr;
+  bpf_printk("destination ip is: %u\n", ip_dst);
 
 	int pkt_sz = data_end - data;
-	bpf_printk("[XDP] packet size %d", pkt_sz);
+	bpf_printk("[XDP] packet size: %d", pkt_sz);
 
-  	return XDP_PASS;
+  return XDP_PASS;
 }
+
+
 
 char _license[] SEC("license") = "GPL";
